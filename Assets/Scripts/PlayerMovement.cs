0148f0e0c8061d0f7ac929189c46bbd1a98846d6
DominicBellisario <dlb5102@g.rit.edu>
2025-01-30 14:07:29 -0500

    added acceleration/deceleration to player movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,16 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Splines;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] SplineContainer spline;
-    [SerializeField] float speed;
-    
     int screenWidth;
 
+    [SerializeField] SplineContainer spline;
+    [SerializeField] float maxSpeed;
+    [SerializeField] float easeInAndOutSpeed;
+
+    float currentSpeed = 0f;
     float distancePercentage = 0f;
     float splineLength;
 
@@ -33,7 +36,7 @@ public class PlayerMovement : MonoBehaviour
                 if (distancePercentage > 0)
                 {
                     //go left
-                    MoveAlongPath(-1);
+                    MoveAlongPath(-easeInAndOutSpeed);
                 }
             }
             //if the cursor is on the right side of the screen
@@ -43,8 +46,31 @@ public class PlayerMovement : MonoBehaviour
                 if (distancePercentage < 1)
                 {
                     //go right
-                    MoveAlongPath(1);
+                    MoveAlongPath(easeInAndOutSpeed);
                 }
+            }
+        }
+        //if mouse is not held down
+        else
+        {
+            //slow to a stop if not at start or end
+            if (currentSpeed < -0.1f)
+            {
+                MoveAlongPath(easeInAndOutSpeed);
+            }
+            else if (currentSpeed > 0.1f)
+            {
+                MoveAlongPath(-easeInAndOutSpeed);
+            }
+            else
+            {
+                currentSpeed = 0;
+            }
+
+            //immedietly stop if at start or end
+            if (distancePercentage <= 0 || distancePercentage >= 1)
+            {
+                currentSpeed = 0;
             }
         }
     }
@@ -52,15 +78,28 @@ public class PlayerMovement : MonoBehaviour
     /// <summary>
     /// moves the player along the given spline in a direction
     /// </summary>
-    /// <param name="direction"></param>
-    private void MoveAlongPath(int direction)
+    /// <param name="speedChange"></param>
+    private void MoveAlongPath(float speedChange)
     {
-        distancePercentage += direction * speed * Time.deltaTime / splineLength;
+        //increase/decrease speed if it is not at max or min
+        currentSpeed += speedChange * Time.deltaTime;
+        if (currentSpeed <= -maxSpeed)
+        {
+            currentSpeed = -maxSpeed;
+        }
+        else if (currentSpeed >= maxSpeed)
+        {
+            currentSpeed = maxSpeed;
+        }
+
+        //update the distance percentage and move the player on teh twine to that position
+        distancePercentage += currentSpeed * Time.deltaTime / splineLength;
 
         Vector3 currentPosition = spline.EvaluatePosition(distancePercentage);
         transform.position = currentPosition;
 
-        Vector3 nextPosition = spline.EvaluatePosition(distancePercentage + 0.05f);
+        //if we want the player's rotation to change, use this
+        //Vector3 nextPosition = spline.EvaluatePosition(distancePercentage + 0.05f);
         //Vector3 direction = nextPosition - currentPosition;
         //transform.rotation = Quaternion.LookRotation(direction, transform.up);
     }
