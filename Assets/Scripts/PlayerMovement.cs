fef871a946aca10dfe38ab7580ee18f96517f3a3
DominicBellisario <dlb5102@g.rit.edu>
2025-01-30 12:57:19 -0500

    added spline package and applied it to player movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,44 +2,66 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Splines;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] SplineContainer spline;
+    [SerializeField] float speed;
+    
     int screenWidth;
-    int screenHeight;
 
-    Rigidbody2D rb;
+    float distancePercentage = 0f;
+    float splineLength;
 
-    [SerializeField]
-    float walkForce;
-    [SerializeField]
-    float maxSpeed;
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
         screenWidth = Screen.width;
-        screenHeight = Screen.height;
+        splineLength = spline.CalculateLength();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //player moves left if they press the left side of the screen, and right if they press the right side
+        //if the mouse is held down
         if (Input.GetMouseButton(0))
         {
+            //if the cursor is on the left side of the screen
             if (Input.mousePosition.x < screenWidth / 2)
             {
-                rb.AddForce(new Vector2(-walkForce * Time.deltaTime, 0));
+                //if the player is not at the start of the path
+                if (distancePercentage > 0)
+                {
+                    //go left
+                    MoveAlongPath(-1);
+                }
             }
+            //if the cursor is on the right side of the screen
             else
             {
-                rb.AddForce(new Vector2(walkForce * Time.deltaTime, 0));
+                //if the player is not at the end of the path
+                if (distancePercentage < 1)
+                {
+                    //go right
+                    MoveAlongPath(1);
+                }
             }
         }
+    }
 
-        //player can not move faster than max speed
-        rb.linearVelocity = Vector2.ClampMagnitude(rb.linearVelocity, maxSpeed);
+    /// <summary>
+    /// moves the player along the given spline in a direction
+    /// </summary>
+    /// <param name="direction"></param>
+    private void MoveAlongPath(int direction)
+    {
+        distancePercentage += direction * speed * Time.deltaTime / splineLength;
+
+        Vector3 currentPosition = spline.EvaluatePosition(distancePercentage);
+        transform.position = currentPosition;
+
+        Vector3 nextPosition = spline.EvaluatePosition(distancePercentage + 0.05f);
+        //Vector3 direction = nextPosition - currentPosition;
+        //transform.rotation = Quaternion.LookRotation(direction, transform.up);
     }
 }
