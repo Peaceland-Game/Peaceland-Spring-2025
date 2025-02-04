17c1fc652cbbe7c989865bc56c6bf62f0c3d8245
DominicBellisario <dlb5102@g.rit.edu>
2025-02-04 14:08:19 -0500

    added a way for the player's max speed to change at chosen points on the spline

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,22 +8,43 @@ using UnityEngine.Splines;
 public class PlayerMovement : MonoBehaviour
 {
     int screenWidth;
+    //the spline container
+    [SerializeField] SplineContainer splineContainer;
+    //the spline within the spline container
+    Spline spline;
+    //the distance percentage of each knot on the spline
+    public float[] knotDistancePercentages;
 
-    [SerializeField] SplineContainer spline;
-    [SerializeField] float maxSpeed;
+    //speed values
+    [SerializeField] float walkSpeed;
+    [SerializeField] float ladderSpeed;
     [SerializeField] float easeInAndOutSpeed;
 
+    //the speed the player has this frame
     float currentSpeed = 0f;
+    //the speed the player is trying to get to
+    float currentDesiredSpeed;
+    //the current distance percentage of the player
     float distancePercentage = 0f;
     float splineLength;
-    Vector3 tangent;
 
     private void Start()
     {
+        //assign variables
         screenWidth = Screen.width;
-        splineLength = spline.CalculateLength();
-        tangent = spline.EvaluateTangent(1);
-        Debug.Log(tangent);
+        spline = splineContainer[0];
+        splineLength = splineContainer.CalculateLength();
+        currentDesiredSpeed = walkSpeed;
+
+        //get the distance percentages for each knot
+        float currentDistance = 0f;
+        knotDistancePercentages = new float[spline.Count];
+        knotDistancePercentages[0] = 0f;
+        for (int i = 1; i < spline.Count; i++)
+        {
+            currentDistance += Vector3.Distance(spline[i - 1].Position, spline[i].Position);
+            knotDistancePercentages[i] = currentDistance / splineLength;
+        }
     }
 
     // Update is called once per frame
@@ -76,6 +97,17 @@ public class PlayerMovement : MonoBehaviour
                 currentSpeed = 0;
             }
         }
+
+        //example of going slower in between 2 knots of the path
+        //when the player is between knots 1 and 2 (a ladder), go slower
+        if (distancePercentage > knotDistancePercentages[2] && distancePercentage < knotDistancePercentages[3])
+        {
+            currentDesiredSpeed = ladderSpeed / 2;
+        }
+        else
+        {
+            currentDesiredSpeed = walkSpeed;
+        }
     }
 
     /// <summary>
@@ -86,13 +118,13 @@ public class PlayerMovement : MonoBehaviour
     {
         //increase/decrease speed if it is not at max or min
         currentSpeed += speedChange * Time.deltaTime;
-        if (currentSpeed <= -maxSpeed)
+        if (currentSpeed <= -currentDesiredSpeed)
         {
-            currentSpeed = -maxSpeed;
+            currentSpeed = -currentDesiredSpeed;
         }
-        else if (currentSpeed >= maxSpeed)
+        else if (currentSpeed >= currentDesiredSpeed)
         {
-            currentSpeed = maxSpeed;
+            currentSpeed = currentDesiredSpeed;
         }
 
         //update the distance percentage and move the player on teh twine to that position
@@ -100,10 +132,5 @@ public class PlayerMovement : MonoBehaviour
 
         Vector3 currentPosition = spline.EvaluatePosition(distancePercentage);
         transform.position = currentPosition;
-
-        //if we want the player's rotation to change, use this
-        //Vector3 nextPosition = spline.EvaluatePosition(distancePercentage + 0.05f);
-        //Vector3 direction = nextPosition - currentPosition;
-        //transform.rotation = Quaternion.LookRotation(direction, transform.up);
     }
 }
