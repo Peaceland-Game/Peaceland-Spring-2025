44298e0ff837491447284a2a2307f6e85393d68f
zxjordan5 <zxjordan5@gmail.com>
2025-02-10 17:10:52 -0500

    Functional dethorning
    Cleaned up some old code, switched to new Unity Event Input System

--- Assets/Scripts/GrabAndSwipe.cs
+++ Assets/Scripts/GrabAndSwipe.cs
@@ -1,11 +1,14 @@
 using System.Runtime.CompilerServices;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class GrabAndSwipe : MonoBehaviour
 {
     //the speed the shears need to be moving in order to slice
     [SerializeField] float sliceSpeed;
+
+    bool isMouseDown;
     bool isSlicing;
     Vector2 previousMousePos;
     SpriteRenderer spriteRenderer;
@@ -19,19 +22,27 @@ public class GrabAndSwipe : MonoBehaviour
         spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-    private void OnMouseDrag()
+    private void Update()
     {
-        transform.position = new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y, 0);
-        if (Vector2.Distance(previousMousePos, transform.position) >= sliceSpeed * Time.deltaTime)
+        if(isMouseDown)
         {
-            isSlicing = true;
-            spriteRenderer.color = Color.red;
+            // Snap to mouse position
+            transform.position = new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y, 0);
+
+            // Determine if the blade is slicing
+            isSlicing = Vector2.Distance(previousMousePos, transform.position) >= sliceSpeed * Time.deltaTime;
+
+            // Update prev mouse pos
+            previousMousePos = transform.position;
         }
-        else
-        {
-            isSlicing = false;
-            spriteRenderer.color = Color.white;
-        }
-        previousMousePos = transform.position;
+    }
+
+    /// <summary>
+    /// Called when user taps down on the screen
+    /// </summary>
+    /// <param name="context"></param>
+    public void OnTap(InputAction.CallbackContext context)
+    {
+        isMouseDown = !context.canceled;
     }
 }
