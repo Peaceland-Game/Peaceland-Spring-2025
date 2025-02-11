4c2fc59b10140e184adaedd74f2f24fd3d0450b8
DominicBellisario <dlb5102@g.rit.edu>
2025-02-11 13:40:48 -0500

    added back accuracy changes

--- Assets/Scripts/FlowerLogic.cs
+++ Assets/Scripts/FlowerLogic.cs
@@ -6,6 +6,11 @@ public class FlowerLogic : MonoBehaviour
     [SerializeField] float gravScale;
     [SerializeField] float popForce;
     [SerializeField] GameObject centerOfGravity;
+    [SerializeField] float pointTime;
+    [SerializeField] LineRenderer guideLine;
+    [SerializeField] LineRenderer cutLine;
+    float timer = 0;
+
 
     private Rigidbody2D rb;
     private Rigidbody2D centerRB;
@@ -30,4 +35,52 @@ public class FlowerLogic : MonoBehaviour
             cut = true;
         }
     }
+
+    private void OnTriggerStay2D(Collider2D collider)
+    {
+        if (collider.CompareTag("Shears"))
+        {
+            timer += Time.deltaTime;
+            if (timer >= pointTime)
+            {
+                timer = 0;
+                cutLine.positionCount++;
+                cutLine.SetPosition(cutLine.positionCount - 1, collider.gameObject.transform.position);
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collider)
+    {
+        if (collider.CompareTag("Shears"))
+        {
+            float averageDistance = 0;
+            //gets the distance of each point on the drawn line
+            for (int i = 0; i < cutLine.positionCount; i++)
+            {
+                averageDistance += LinePointDistance(guideLine.GetPosition(0), guideLine.GetPosition(1), cutLine.GetPosition(i));
+            }
+            //calculate the average
+            averageDistance /= cutLine.positionCount;
+            Debug.Log("Average distance: " + averageDistance);
+
+            //reset cut line
+            cutLine.positionCount = 0;
+        }
+    }
+
+    /// <summary>
+    /// returns the distance between a point and a line
+    /// </summary>
+    /// <param name="lineStart"></param>
+    /// <param name="lineEnd"></param>
+    /// <param name="point"></param>
+    /// <returns></returns>
+    private float LinePointDistance(Vector2 lineStart, Vector2 lineEnd, Vector2 point)
+    {
+        Vector2 lineDirection = lineEnd - lineStart;
+        Vector2 pointToLineStart = point - lineStart;
+        return Vector3.Cross(pointToLineStart, lineDirection).magnitude / lineDirection.magnitude;
+    }
+
 }
