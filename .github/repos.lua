ff211f1d9b31912df6e6446fb00072982f80d3d2
Julian <julianheuser@outlook.com>
2025-02-11 13:49:37 -0500

    Update action

--- .github/repos.lua
+++ .github/repos.lua
@@ -1,13 +1,13 @@
 local config = {
-    labels_fetch = "repo-fetch,dependencies"
-    labels_update = "dependencies",
+    labels_fetch = "repo-fetch,dependencies",
+    labels_update = "dependencies"
 }
 
 local repos = {
     {
-        name = "Yarn",
+        name = "YarnProjects",
         url = "https://github.com/Peaceland-Game/Yarn-Spring-2025.git",
-        dir = "Assets/Yarn/YarnProjects",
+        dir = "Assets/Yarn/",
         def_branch = "main"
     }
 }
