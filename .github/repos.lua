f12a33368ad7f680e4a11c255df23ffbf4c592e9
jeh8035 <78885171+jeh8035@users.noreply.github.com>
2025-02-11 13:25:36 -0500

    Add github action (#9)
    Co-authored-by: Julian <julianheuser@outlook.com>

---
+++ .github/repos.lua
@@ -0,0 +1,19 @@
+local config = {
+    labels_fetch = "repo-fetch,dependencies"
+    labels_update = "dependencies",
+}
+
+local repos = {
+    {
+        name = "Yarn",
+        url = "https://github.com/Peaceland-Game/Yarn-Spring-2025.git",
+        dir = "Assets/Yarn/YarnProjects",
+        def_branch = "main"
+    }
+}
+
+-- Fully needed, so that the scripts can access the configurations.
+return {
+    repos = repos,
+    config = config
+}
