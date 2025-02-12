5a3d25f4e537ca9232f602949a68a678e5081d11
Julian <julianheuser@outlook.com>
2025-02-12 11:33:20 -0500

    Add drag targets

--- Assets/Scripts/DraggableFlower.cs
+++ Assets/Scripts/DraggableFlower.cs
@@ -1,7 +1,11 @@
+using System;
 using UnityEngine;
 
-public class Draggable : MonoBehaviour
+public class DraggableFlower : MonoBehaviour
 {
+    [SerializeField]
+    private Transform dragTarget;
+
     private bool dragging = false;
     private Vector3 offset;
     
@@ -36,6 +40,12 @@ public class Draggable : MonoBehaviour
         else if (dragging && touch.phase == TouchPhase.Moved)
         {
             transform.position = new Vector3(touch_wp.x + offset.x, touch_wp.y + offset.y, transform.position.z);
+
+            float dist = (transform.position - dragTarget.position).magnitude;
+            if (dist < 2.0) {
+                transform.position = dragTarget.position;
+            }
+
         }
         else if (dragging && (touch.phase == TouchPhase.Ended || touch.phase == TouchPhase.Canceled))
         {
