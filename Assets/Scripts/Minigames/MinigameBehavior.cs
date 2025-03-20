fabb473263eaf801135825df5cb7656ae4be76b5
MrBananaMan541 <112341486+MrBananaMan541@users.noreply.github.com>
2025-03-20 14:41:14 -0400

    Fixed touch input not working, and start on handling flowers with different minigame aspects (#41)

--- Assets/Scripts/Minigames/MinigameBehavior.cs
+++ Assets/Scripts/Minigames/MinigameBehavior.cs
@@ -3,6 +3,12 @@ using UnityEngine;
 
 public abstract class MinigameBehavior : MonoBehaviour
 {
+    /// <summary>
+    /// Allows children to overwrite and decide their own start methods
+    /// </summary>
     public abstract void StartMinigame();
+    /// <summary>
+    /// Allows children to overwrite and decide their own stop methods
+    /// </summary>
     public abstract void StopMinigame();
 }
