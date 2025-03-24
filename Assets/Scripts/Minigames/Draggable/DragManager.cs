61602b4b179aa6dda1e74224f0b0d493fc6243c6
MrBananaMan541 <mrbananaman541@gmail.com>
2025-03-24 17:44:06 -0400

    Fixed arranging not resetting and breaking, also transitioning between minigames and multiple flowers

--- Assets/Scripts/Minigames/Draggable/DragManager.cs
+++ Assets/Scripts/Minigames/Draggable/DragManager.cs
@@ -14,9 +14,42 @@ public class DragManager : MinigameBehavior
     /// </summary>
     private int flowerArrangeNum = 0;
 
+    /// <summary>
+    /// The starting positions of the flowers to reset them to each time this is restarted
+    /// </summary>
+    private Vector3[] startingLocations;
+
+    private void InitializeStartingLocations()
+    {
+        //Initialize the startingLocations array
+        startingLocations = new Vector3[draggables.Length];
+
+        //Set the starting location of each flower to save when restarting the arranging minigame
+        for (int i = 0; i < draggables.Length; i++)
+        {
+            startingLocations[i] = draggables[i].gameObject.transform.localPosition;
+        }
+    }
+
     public override void StartMinigame()
     {
+        //Reset the current draggable
         currentDraggable = null;
+
+        //If the startingLocations array has not been initialized and filled, then do it now
+        if (startingLocations == null || startingLocations.Length == 0)
+        {
+            InitializeStartingLocations();
+        }
+
+        //For each flower, reset its position to its starting location and make sure they can be dragged
+        for (int i = 0; i < draggables.Length; i++)
+        {
+            draggables[i].gameObject.transform.localPosition = startingLocations[i];
+            draggables[i].EnableDrag();
+        }
+
+        //Set the arranging minigame to active
         gameObject.SetActive(true);
     }
 
@@ -31,8 +64,11 @@ public class DragManager : MinigameBehavior
         if (context.phase == InputActionPhase.Disabled || context.phase == InputActionPhase.Canceled) {
             if (currentDraggable is not null)
             {
+                //End the drag of the current draggable
+                currentDraggable.EndDrag();
+
                 //If the number of flowers that have been arranged is less than the length of the draggables array, continue
-                if (flowerArrangeNum < draggables.Length)
+                if (flowerArrangeNum < draggables.Length && !currentDraggable.IsDraggable())
                 {
                     //Increment the num of flowers arranged
                     flowerArrangeNum++;
@@ -45,9 +81,6 @@ public class DragManager : MinigameBehavior
                         FlowerShopManager.Instance.NextMinigame();
                     }
                 }
-
-                //End the drag of the current draggable
-                currentDraggable.EndDrag();
             }
         }
         else if (context.phase == InputActionPhase.Started){
