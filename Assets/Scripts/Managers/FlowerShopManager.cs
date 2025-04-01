624cc9a6030a280b992eb1f50649265896b73405
MrBananaMan541 <mrbananaman541@gmail.com>
2025-04-01 18:07:35 -0400

    Dethorn and trimming now handle flowers needing or not needing to be dethorned or trimmed

--- Assets/Scripts/Managers/FlowerShopManager.cs
+++ Assets/Scripts/Managers/FlowerShopManager.cs
@@ -67,17 +67,30 @@ public class FlowerShopManager : MonoBehaviour
         return Instance.orders[currentOrder];
     }
 
-    public static void NextOrder() {
+    /// <summary>
+    /// Increment the current order and go to the next one
+    /// </summary>
+    public static void NextOrder()
+    {
         currentOrder++;
+    }
+
+    /// <summary>
+    /// The index of the current minigame that this memory is on
+    /// </summary>
+    /// <returns>The index of the current minigame this memory is on</returns>
+    public static int GetCurrentMinigameIndex()
+    {
+        return Instance.currentMinigame;
     }
 
     /// <summary>
     /// The current minigame that this memory is on
     /// </summary>
     /// <returns>The current minigame this memory is on</returns>
-    public static int GetCurrentMinigame()
+    public static MinigameBehavior GetCurrentMinigame()
     {
-        return Instance.currentMinigame;
+        return Instance.minigames[GetCurrentMinigameIndex()];
     }
 
     /// <summary>
@@ -95,10 +108,10 @@ public class FlowerShopManager : MonoBehaviour
     /// <param name="index">The current flower to pull the appropriate game object from</param>
     /// <param name="minigame">The current minigame to test for</param>
     /// <returns>Flower stem if the minigame is dethorning, and whole flower if the minigame is trimming</returns>
-    public static GameObject ReturnGameObjectBasedOnMinigame(int index, int minigame)
+    public static GameObject ReturnGameObjectBasedOnMinigame(int index, string minigame)
     {
         //If the minigame is dethorning, return the flower stem
-        if (minigame == 1)
+        if (minigame == "Dethorn")
         {
             return GetCurrentOrder().flowers[index].flowerStem;
         }
@@ -109,6 +122,11 @@ public class FlowerShopManager : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Gets the correct flower sprite that matches the given flower type
+    /// </summary>
+    /// <param name="t">The given flower type to search for</param>
+    /// <returns>The flower sprite that matches the given one</returns>
     public static Sprite GetFlowerSprite(FlowerType t)
     {
         return Instance.flowerSprites[(int)t];
@@ -129,7 +147,31 @@ public class FlowerShopManager : MonoBehaviour
     public void NextMinigame()
     {
 
-        if (currentMinigame >= 0) minigames[currentMinigame].StopMinigame();
+        //As long as the current minigame is at a valid index (greater than 0), stop the minigame at that index
+        if (currentMinigame >= 0 && currentFlower == 0) minigames[currentMinigame].StopMinigame();
+
+        //If the current step in the current flower for the current order is not needed, then continue to the next minigame
+        if ((!GetCurrentFlower(currentFlower).needsDethorning && minigames[currentMinigame + 1].gameObject.name == "Dethorn") ||
+            (!GetCurrentFlower(currentFlower).needsTrimming && minigames[currentMinigame + 1].gameObject.name == "Trimming"))
+        {
+            //Increment the current flower this minigame is on
+            currentFlower++;
+
+            //If the next flower is out of range, then go back to the first flower
+            if (currentFlower >= GetCurrentOrder().flowers.Count)
+            {
+                currentFlower = 0;
+                CutManager.CurIndex = currentFlower;
+            }
+            else
+            {
+                //Update the flower index in the CutManager, and then set it up for cutting
+                CutManager.CurIndex = currentFlower;
+                CutManager.CutStart();
+            }
+
+            NextMinigame();
+        }
 
         //Increment to the next minigame
         currentMinigame++;
@@ -137,7 +179,12 @@ public class FlowerShopManager : MonoBehaviour
         //If the current minigame is higher or equal to the number of minigames, continue
         if (currentMinigame >= minigames.Count)
         {
+            //Reset the current minigame num, the current order num, and the current flower num (basically reset everything back
+            //to the beginning of the memory
             currentMinigame = 0;
+            currentOrder = 0;
+            currentFlower = 0;
+            CutManager.CurIndex = currentFlower;
             // TODO: W're done, end the game (or memory)!!!
         }
 
--- Assets/Scripts/Minigames/CutManager.cs
+++ Assets/Scripts/Minigames/CutManager.cs
@@ -10,6 +10,12 @@ public class CutManager : MinigameBehavior
 {
     private static int curIndex = 0;
 
+    public static int CurIndex
+    {
+        get { return curIndex; }
+        set { curIndex = value; }
+    }
+
     private static CutManager instance;
 
     private static GameObject instantiatedFlower;
@@ -29,7 +35,7 @@ public class CutManager : MinigameBehavior
     public static void CutStart()
     {
         instantiatedFlower = Instantiate(FlowerShopManager.ReturnGameObjectBasedOnMinigame(curIndex,
-            FlowerShopManager.GetCurrentMinigame()));
+            FlowerShopManager.GetCurrentMinigame().gameObject.name));
     }
 
 
@@ -40,20 +46,36 @@ public class CutManager : MinigameBehavior
         curIndex++;
         FlowerShopManager.currentFlower = curIndex;
 
+        //As long as the next flower in the list for this order exists, continue below
         if (curIndex < FlowerShopManager.GetCurrentOrder().flowers.Count)
         {
-            if ((FlowerShopManager.GetCurrentFlower(curIndex).needsDethorning && FlowerShopManager.GetCurrentMinigame() == 1) ||
-            (FlowerShopManager.GetCurrentFlower(curIndex).needsTrimming && FlowerShopManager.GetCurrentMinigame() == 2))
+            //If the current flower needs to be dethorned and the current minigame is dethorning, or the current flower needs to be
+            //trimmed and the current minigame is trimming, then set up the next flower
+            if ((FlowerShopManager.GetCurrentFlower(curIndex).needsDethorning && FlowerShopManager.GetCurrentMinigame().gameObject.name == "Dethorn") ||
+            (FlowerShopManager.GetCurrentFlower(curIndex).needsTrimming && FlowerShopManager.GetCurrentMinigame().gameObject.name == "Trimming"))
             {
                 CutStart();
             }
-            else
+            //Otherwise if the current flower is the last one in the list, go back to the first flower in the list
+            else if (curIndex == FlowerShopManager.GetCurrentOrder().flowers.Count - 1)
             {
                 curIndex = 0;
+
+                //Update the current flower in the FlowerShopManager, and move to the next minigame
                 FlowerShopManager.currentFlower = curIndex;
                 FlowerShopManager.Instance.NextMinigame();
             }
+            //And if neither of the statements above are true, then move to the next flower
+            else
+            {
+                curIndex++;
+
+                //Update the current flower in the FlowerShopManager, and set up the next flower
+                FlowerShopManager.currentFlower = curIndex;
+                CutStart();
+            }
         }
+        //Otherwise move to the next minigame
         else
         {
             curIndex = 0;
