bd601d33ec32f8d6338d4716ee295ef3f7a884a4
Justin <90058544+JustinBaez8@users.noreply.github.com>
2025-04-21 15:46:41 -0400

    Adjusting Dialogue UI & QOL Changes (#71)

---
+++ Assets/RestartGame.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartGame : MonoBehaviour
+{
+    [SerializeField]
+    private float transitionTime = 5f;
+
+    void Start()
+    {
+        Invoke("StartGame", transitionTime);
+    }
+
+    void StartGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -7,11 +7,6 @@ public class LevelLoader : MonoBehaviour
     public Animator transition;
 
     public float transitionTime = 1f;
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
 
     public void QuitGame()
     {
