e168f764418ca41e7af32b2b27df27d3cbc95eb0
Billy Guzik (RIT Student) <bjg9690@rit.edu>
2025-05-28 16:08:21 -0400

    GameManager.cs
    can be found in Scripts -> Managers folder

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+//data that needs to be stored globally and transfered between scenes can be stored and referenced in this script
+public class GameManager : MonoBehaviour
+{
+    //variables
+
+    //create the private instance
+    private static GameManager _instance;
+
+    //create the public reference
+    public static GameManager Instance
+    {
+        get
+        {
+            if (_instance is null)
+                Debug.LogError("GameManager is NULL");
+
+            return _instance;
+        }
+    }
+
+    //initialize private instance
+    void Start()
+    {
+        _instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
