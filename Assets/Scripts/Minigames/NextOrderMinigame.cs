066f28b9198478976eb402ed6ece29bcf69df48a
Jason Weinberg (RIT Student) <jpw3667@rit.edu>
2025-06-11 16:39:56 -0400

    people transition timer

--- Assets/Scripts/Minigames/NextOrderMinigame.cs
+++ Assets/Scripts/Minigames/NextOrderMinigame.cs
@@ -10,16 +10,34 @@ public class NextOrderMinigame : MinigameBehavior
 {
     [SerializeField]
     private GameObject characterPortrait;
+    private float pauseTimer;
 
     public override void StartMinigame()
     {
-        characterPortrait.GetComponent<SpriteRenderer>().sprite = FlowerShopManager.NextOrderChar();
-        gameObject.SetActive(true);
-        FlowerShopManager.Instance.NextMinigame();
+        pauseTimer = 2;
+        characterPortrait.SetActive(false);
+
+
     }
 
     public override void StopMinigame()
     {
         gameObject.SetActive(false);
+    }
+
+    //Added an update method to run a timer in between characters
+    public void Update()
+    {
+        if(pauseTimer > 0)
+        {
+            pauseTimer -= Time.deltaTime;
+            if(pauseTimer < 0)
+            {
+                characterPortrait.SetActive(true);
+                characterPortrait.GetComponent<SpriteRenderer>().sprite = FlowerShopManager.NextOrderChar();
+                gameObject.SetActive(true);
+                FlowerShopManager.Instance.NextMinigame();
+            }
+        }   
     }
 }
