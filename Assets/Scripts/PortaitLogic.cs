b977e45681d155037fa6373415cc2a866d20a606
rbs5340 <rbs5340@g.rit.edu>
2025-06-12 11:26:40 -0400

    TwoCharacterSupport V1

--- Assets/Scripts/PortaitLogic.cs
+++ Assets/Scripts/PortaitLogic.cs
@@ -8,22 +8,66 @@ public class PortaitLogic : MonoBehaviour
     private DialogueRunner dialogueRunner;
 
     [SerializeField]
-    private SpriteRenderer characterSprite;
+    private GameObject characterPortrait;
+
+    [SerializeField]
+    private GameObject secondCharacterPortrait;
 
     public void Awake()
     {
         //Adds commands that can be called in yarn using <<lighten>> and <<darken>> respectively
         dialogueRunner.AddCommandHandler("lighten", Lighten);
         dialogueRunner.AddCommandHandler("darken", Darken);
+        dialogueRunner.AddCommandHandler<int>("darken2", Darken2);
+        dialogueRunner.AddCommandHandler<int>("lighten2", Lighten2);
+        dialogueRunner.AddCommandHandler("oneChar", OneChar);
+        dialogueRunner.AddCommandHandler("twoChar", TwoChar);
     }
 
     private void Darken()
     {
-        characterSprite.color = new Color(.75f, .75f, .75f, 1f);
+        characterPortrait.GetComponent<SpriteRenderer>().color = new Color(.75f, .75f, .75f, 1f);
     }
 
     private void Lighten()
     {
-        characterSprite.color = Color.white;
+        characterPortrait.GetComponent<SpriteRenderer>().color = Color.white;
+    }
+
+    private void Darken2(int i)
+    {
+        if (i == 1)
+        {
+            characterPortrait.GetComponent<SpriteRenderer>().color = new Color(.75f, .75f, .75f, 1f);
+        }
+        else
+        {
+            secondCharacterPortrait.GetComponent<SpriteRenderer>().color = new Color(.75f, .75f, .75f, 1f);
+        }
+    }
+
+    private void Lighten2(int i)
+    {
+        if (i == 1)
+        {
+            characterPortrait.GetComponent<SpriteRenderer>().color = Color.white;
+        }
+        else
+        {
+            secondCharacterPortrait.GetComponent<SpriteRenderer>().color = Color.white;
+        }
+    }
+
+    private void OneChar()
+    {
+        characterPortrait.transform.position = new Vector3(-3, -1, 0);
+        secondCharacterPortrait.SetActive(false);
+    }
+
+    private void TwoChar()
+    {
+        characterPortrait.transform.position = new Vector3(-5, -1, 0);
+        secondCharacterPortrait.transform.position = new Vector3(5, -1, 0);
+        secondCharacterPortrait.SetActive(true);
     }
 }
