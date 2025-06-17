3e2817ec7015007a34ac21625e6bc0b5ebc400e3
Jason Weinberg (RIT Student) <jpw3667@rit.edu>
2025-06-17 16:18:07 -0400

    dethorning update polished

--- Assets/Scripts/ThornyFlowerZoom.cs
+++ Assets/Scripts/ThornyFlowerZoom.cs
@@ -1,26 +1,36 @@
 using UnityEditor;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class ThornyFlowerZoom : MonoBehaviour
 {
+    private float zoomDelay = 1f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        this.transform.position = new Vector3(-3.3f, -0.3f, 1f);
+        this.transform.position = new Vector3(0f, -0.3f, 1f);
         this.transform.localScale = new Vector3(0.4f, 0.4f, 1f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.localScale = (Vector3)transform.localScale * (1.001f);
-        transform.position = new Vector3(transform.position.x - (0.001f * transform.localScale.x), transform.position.y, this.transform.position.z);
-        if (transform.localScale.x > 5)
-        {
-            Debug.Log("spawn");
-            CutManager cm = new CutManager();
-            cm.beginDethorn();
-            this.gameObject.SetActive(false);
+        if (zoomDelay < 0f)
+            {
+            transform.localScale = (Vector3)transform.localScale * (1.001f);
+            transform.position = new Vector3(transform.position.x - (0.0016f * transform.localScale.x), transform.position.y, this.transform.position.z);
+            if (transform.localScale.x > 5)
+            {
+                Debug.Log("spawn");
+                CutManager cm = new CutManager();
+                cm.beginDethorn();
+                this.gameObject.SetActive(false);
+
+            }
         }
+        else {
+            zoomDelay -= Time.deltaTime;
+        }
+
     }
 }
