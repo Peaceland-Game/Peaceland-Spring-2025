74a2f11d600df7f4d99536460adacc9b4e078dd8
Billy Guzik (RIT Student) <bjg9690@rit.edu>
2025-07-01 16:58:36 -0400

    MuseumIntro Stuff
    Yarnspinner dialogue
    
    MuseumIntroManager.cs
    
    mira import

---
+++ Assets/Scripts/Managers/MuseumIntroManager.cs
@@ -0,0 +1,71 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Yarn.Unity;
+
+public class MuseumIntroManager : MonoBehaviour
+{
+    //List of "minigames"
+    [SerializeField]
+    private List<MinigameBehavior> minigames;
+    private int currentMinigame = -1;
+  
+    // Singleton Instace
+    public static MuseumIntroManager Instance { get; private set; }
+
+    [SerializeField]
+    private DialogueRunner dialogueRunner;
+
+    /// <summary>
+    /// The index of the current minigame that this memory is on
+    /// </summary>
+    /// <returns>The index of the current minigame this memory is on</returns>
+    public static int GetCurrentMinigameIndex()
+    {
+        return Instance.currentMinigame;
+    }
+
+    /// <summary>
+    /// The current minigame that this memory is on
+    /// </summary>
+    /// <returns>The current minigame this memory is on</returns>
+    public static MinigameBehavior GetCurrentMinigame()
+    {
+        return Instance.minigames[GetCurrentMinigameIndex()];
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Instance = this;
+        dialogueRunner.onDialogueComplete.AddListener(NextMinigame);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// Changes the state of the flower shop memory
+    /// </summary>
+    public void NextMinigame()
+    {
+        //As long as the current minigame is at a valid index (greater than 0), stop the minigame at that index
+        if (currentMinigame >= 0) minigames[currentMinigame].StopMinigame();
+
+        //Increment to the next minigame
+        currentMinigame++;
+
+        //If the current minigame is higher or equal to the number of minigames, play animation and change scene to flower memory
+        if (currentMinigame >= minigames.Count)
+        {
+            
+            return;  
+        }
+
+        //Start the next minigame
+        minigames[currentMinigame].StartMinigame();
+    }
+}
