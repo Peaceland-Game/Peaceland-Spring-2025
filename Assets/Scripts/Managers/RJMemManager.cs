4b15bdb165287434ba37ed1f901fae2b0996e480
EvanFankhauser <epf4946@g.rit.edu>
2026-04-01 17:22:17 -0400

    Built simple demo click-though for R&J memory.
    Currently clicks through rough dialogue, no puzzle minigame yet.
    Utilizes most of the behavior from the previous demo, but combined into one script that inherits from GenericMemManager.cs.
    Currently uses the old art, will update soon.
    
    Known bug: The white "fade out" effect appears behind the character sprite.

--- Assets/Scripts/Managers/RJMemManager.cs
+++ Assets/Scripts/Managers/RJMemManager.cs
@@ -20,4 +20,34 @@ public class RJMemManager : GenericMemManager
         // Connect dialogue runner
         dialogueRunner.onDialogueComplete.AddListener(NextMinigame);
     }
+
+    /// <summary>
+    /// Override methods to go to the next minigame. The logic is mostly identical
+    /// to the base class, but with an added line to check a bool in GameManager
+    /// to avoid repeating dialogue in the present.
+    /// </summary>
+    public override void NextMinigame()
+    {
+        Debug.Log("Current Minigame " + currentMinigame);
+
+        // Stope the current mingame, if there is one.
+        if (currentMinigame >= 0) minigames[currentMinigame].StopMinigame();
+
+        // Increment minigame counter
+        currentMinigame++;
+
+        // If passed all the minigames, reset the Unity scene and go to the next Unity scene
+        if (currentMinigame >= minigames.Count)
+        {
+            ResetMemory();
+            Debug.Log("End Memory");
+
+            // Update GM bool and return to the present
+            GameManager.Instance.seenRJMemory = true;
+            SceneManager.LoadScene(2);
+            return;
+        }
+        // Otherwise, start the next minigame.
+        minigames[currentMinigame].StartMinigame();
+    }
 }
